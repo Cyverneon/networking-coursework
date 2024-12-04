d94609dbcc59956ffda146dca432704522589410
Cyvern <cyverneon@gmail.com>
2024-12-04 23:05:31 +0000

    Start implementing movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,27 @@ using Unity.Netcode;
 
 public class PlayerMovement : NetworkBehaviour
 {
+    [Header("Movement")]
+    [Tooltip("Player speed in units per second")]
     [SerializeField] private float speed = 2f;
+    [Tooltip("Jump height in units")]
+    [SerializeField] private float jumpHeight = 50f;
+    [Tooltip("Time in seconds player can press jump while about to hit the ground and still jump")]
+    [SerializeField] private float jumpLeniency = 0.1f;
 
+    [Tooltip("Varience from upwards angle that is still considered \"ground\" (can jump off) ")]
+    [SerializeField] private float groundAngle = 30;
+
+    private float jumpTimer = 0f;
+    private bool cueJump = false;
+    private bool onGround = true;
+
+    [Header("References")]
+    [SerializeField] private Collider2D collider2d;
+    [SerializeField] private Rigidbody2D rigidBody;
 
     private void Movement(float delta)
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-        }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= new Vector3(speed * delta, 0f, 0f);
@@ -24,14 +34,55 @@ public class PlayerMovement : NetworkBehaviour
         {
             transform.position += new Vector3(speed * delta, 0f, 0f);
         }
+
+        if (cueJump)
+        {
+            if (onGround)
+            {
+                rigidBody.AddForce(Vector2.up * jumpHeight);
+                cueJump = false;
+            }
+        }
+
+        LayerMask layerMask = LayerMask.GetMask("Environment");
+
+        onGround = collider2d.IsTouchingLayers(layerMask);
+/*        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 200.0f, layerMask);
+        if (hit)
+        {
+            Debug.Log("raycast hit");
+        }
+        else
+        {
+            Debug.Log("raycast not hit");
+        }*/
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         if (IsOwner)
         {
-            Movement(Time.fixedDeltaTime);
+            Movement(Time.deltaTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                cueJump = true;
+                jumpTimer = jumpLeniency;
+            }
+            if (jumpTimer > 0)
+            {
+                jumpTimer -= Time.deltaTime;
+                if (jumpTimer < 0)
+                {
+                    cueJump = false;
+                }
+            }
         }
     }
 }
