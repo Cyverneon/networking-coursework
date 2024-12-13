27ec4cdfbf402761320a88c5516346b11a34fdf3
Cyvern <cyverneon@gmail.com>
2024-12-13 16:17:58 +0000

    Add delay to camera following player

--- Assets/Scripts/Player/CamFollowPlayer.cs
+++ Assets/Scripts/Player/CamFollowPlayer.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 
 public class CamFollowPlayer : MonoBehaviour
 {
-    GameObject _player;
+    private GameObject _player;
+    private Vector3 _targetPos;
+
+    [SerializeField] private float _speed;
+
     // Start is called before the first frame update
     void Start()
     {
         _player = NetworkManager.Singleton.LocalClient.PlayerObject.gameObject;
+        _targetPos.z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, transform.position.z);
+        _targetPos.x = _player.transform.position.x;
+        _targetPos.y = _player.transform.position.y;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPos, _speed * Time.deltaTime);
     }
 }
