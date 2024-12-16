e4a06357724d136171ed6cac14ee2959a4bbfb8f
Cyvern <cyverneon@gmail.com>
2024-12-16 22:02:08 +0000

    Fix bug with circle move script

--- Assets/Scripts/Movement/MoveCircle.cs
+++ Assets/Scripts/Movement/MoveCircle.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 
 public class MoveCircle : MonoBehaviour
 {
-    [SerializeField] private float _rotSpeed = 5f;
+    [Tooltip("Speed of movement in degrees per second")]
+    [SerializeField] private float _speed = 180f;
+    [Tooltip("Distance from orbit pos that the object will circle at")]
     [SerializeField] private float _radius = 1f;
-    [SerializeField] private Vector3 _startPos = Vector3.zero;
+    [Tooltip("Center of the circle that the object will move around")]
+    [SerializeField] private Vector3 _orbitPos = Vector3.zero;
+    [Tooltip("Move clockwise or anticlockwise?")]
+    [SerializeField] private bool _clockwise = true;
 
     private float _currentAngle = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = _startPos;
-    }
+    private float _currentAngleRad = 0f;
 
-    // Update is called once per frame
-    void Update()
+    Vector3 GetNextPos()
     {
-        _currentAngle += _rotSpeed * Time.deltaTime;
+        _currentAngle += (_clockwise ? _speed : -_speed) * Time.deltaTime;
         _currentAngle = _currentAngle % 360;
 
+        _currentAngleRad = (_currentAngle * Mathf.PI) / 180;
+
         Vector3 newPos = transform.position;
-        newPos.x = _startPos.x + (Mathf.Sin(_currentAngle)*_radius);
-        newPos.y = _startPos.y + (Mathf.Cos(_currentAngle)*_radius);
-        transform.position = newPos;
+        newPos.x = _orbitPos.x + (Mathf.Sin(_currentAngleRad) * _radius);
+        newPos.y = _orbitPos.y + (Mathf.Cos(_currentAngleRad) * _radius);
+        return newPos;
+    }
+
+    void Update()
+    {
+        transform.position = GetNextPos();
     }
 }
